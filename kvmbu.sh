7e0e5b33994c36b3fa036e50d547e667e8f2abb3
jason.sh <jasonhensley93@gmail.com>
2018-12-28 15:11:18 -0600

    Update kvmbu.sh

--- kvmbu.sh
+++ kvmbu.sh
@@ -3,10 +3,11 @@
 # Created and maintained by https://github.com/JLH993
 # Use at your own risk and check the integrity of your backups often. 
 
-# Define variables
+# Define variables (Change $BKUP to match where your disks will be backed up to.)
 PWROFF="$(virsh list --all | grep "shut off" | awk '{print$2}')"
 BLKLIST=`for i in $PWROFF; do virsh domblklist $i | grep "qcow2" | awk '{print$2}'; done`
 BLSHORT=`echo "$BLKLIST" | awk -F'/' '{print$5}'`
+BKUP="/backups/Linux/kvm/"
 
 # Back up powered off images... 
-for i in $BLKLIST; do tar czvf /backups/Linux/kvm/$(echo $i | awk -F'/' '{print$5}').tgz $i; done
+for i in $BLKLIST; do tar czvf $BKUP$(echo $i | awk -F'/' '{print$5}').tgz $i; done
